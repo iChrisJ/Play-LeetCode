d0eb35cc4876a8a899b9b63a722871a4b8f2319c
iChrisJ <chrisjiang@outlook.com>
2019-08-01 23:02:53 +0800

    feat: leetcode 1.

---
+++ CSharp/CSharp/0001 Two Sum.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp._0001_Two_Sum
+{
+	public class Solution
+	{
+		public int[] TwoSum(int[] nums, int target)
+		{
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (dict.ContainsKey(target - nums[i]))
+					return new int[] { (int)dict[target - nums[i]], i };
+				else
+				{
+					if (dict.ContainsKey(nums[i]))
+						dict[nums[i]] = i;
+					else
+						dict.Add(nums[i], i);
+				}
+			}
+
+			return null;
+		}
+
+		static void Main(string[] args)
+		{
+			var a = new Solution().TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+		}
+	}
+
+
+}
