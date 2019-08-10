df096e8e867a683c0ff006f26776afdbc9dcb403
iChrisJ <chrisjiang@outlook.com>
2019-08-10 13:38:25 +0800

    feat: leetcode 51, 52. NQueen, backtracking

---
+++ CSharp/CSharp/0051 N-Queens.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp._0051_N_Queens
+{
+	public class Solution
+	{
+		private bool[] col, dia1, dia2;
+
+		public IList<IList<string>> Result { get; set; }
+
+		public IList<IList<string>> SolveNQueens(int n)
+		{
+			Result = new List<IList<string>>();
+			col = new bool[n];
+			dia1 = new bool[2 * n - 1];
+			dia2 = new bool[2 * n - 1];
+			PutQueen(n, 0, new List<int>());
+			return Result;
+		}
+
+		private void PutQueen(int n, int index, IList<int> row)
+		{
+			if (index == n)
+			{
+				Result.Add(GenerateBoard(n, row));
+				return;
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				if (!col[i] && !dia1[index + i] && !dia2[index - i + n - 1])
+				{
+					row.Add(i);
+					col[i] = true;
+					dia1[index + i] = true;
+					dia2[index - i + n - 1] = true;
+					PutQueen(n, index + 1, row);
+					col[i] = false;
+					dia1[index + i] = false;
+					dia2[index - i + n - 1] = false;
+					row.RemoveAt(row.Count - 1);
+				}
+			}
+		}
+
+		private IList<string> GenerateBoard(int n, IList<int> row)
+		{
+			IList<string> board = new List<string>(n);
+			for (int i = 0; i < n; i++)
+			{
+				StringBuilder sb = new StringBuilder(n);
+				for (int j = 0; j < n; j++)
+					sb.Append(row[i] == j ? 'Q' : '.');
+				board.Add(sb.ToString());
+			}
+			return board;
+		}
+
+		//public static void Main()
+		//{
+		//	var res = new Solution().SolveNQueens(4);
+		//}
+	}
+}
---
+++ CSharp/CSharp/0052 N-Queens II.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp._0052_N_Queens_II
+{
+	// Almost same as the leetcode 51 and easier than it.
+	public class Solution
+	{
+		private bool[] col, dia1, dia2;
+
+		public int Result { get; set; }
+
+		public int TotalNQueens(int n)
+		{
+			Result = 0;
+			col = new bool[n];
+			dia1 = new bool[2 * n - 1];
+			dia2 = new bool[2 * n - 1];
+			PutQueen(n, 0, new List<int>());
+			return Result;
+		}
+
+		private void PutQueen(int n, int index, IList<int> row)
+		{
+			if (index == n)
+			{
+				Result++;
+				return;
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				if (!col[i] && !dia1[index + i] && !dia2[index - i + n - 1])
+				{
+					row.Add(i);
+					col[i] = true;
+					dia1[index + i] = true;
+					dia2[index - i + n - 1] = true;
+					PutQueen(n, index + 1, row);
+					col[i] = false;
+					dia1[index + i] = false;
+					dia2[index - i + n - 1] = false;
+					row.RemoveAt(row.Count - 1);
+				}
+			}
+		}
+	}
+}
