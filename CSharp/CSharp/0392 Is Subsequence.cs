a41069ade19e975dba288a183ac728ccf10e5198
iChrisJ <chrisjiang@outlook.com>
2019-08-27 23:38:14 +0800

    feat: leetcode 392, 744, binary search

---
+++ CSharp/CSharp/0392 Is Subsequence.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharp._0392_Is_Subsequence
+{
+	public class Solution
+	{
+		public bool IsSubsequence(string s, string t)
+		{
+			if (string.IsNullOrEmpty(s))
+				return true;
+			if (string.IsNullOrEmpty(t))
+				return false;
+
+			int i = 0, j = 0;
+
+			while (j < s.Length && i < t.Length)
+			{
+				if (s[j] == t[i])
+				{
+					i++;
+					j++;
+				}
+				else
+					i++;
+			}
+
+			return j == s.Length;
+		}
+	}
+}
---
+++ CSharp/CSharp/0744 Find Smallest Letter Greater Than Target.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSharp._0744_Find_Smallest_Letter_Greater_Than_Target
+{
+	public class Solution
+	{
+		public char NextGreatestLetter(char[] letters, char target)
+		{
+			if (letters == null || letters.Length == 0)
+				throw new Exception("Incorrect input!");
+
+			int l = 0, r = letters.Length - 1;
+			while (l <= r)
+			{
+				int mid = l + (r - l) / 2;
+				if (letters[mid] == target)
+				{
+					if (mid == letters.Length - 1)
+						return letters[0];
+					l = mid + 1;
+					break;
+				}
+				else if (letters[mid] > target)
+					r = mid - 1;
+				else
+					l = mid + 1;
+			}
+
+			if (l == letters.Length)
+				return letters[0];
+			while (l < letters.Length && letters[l] == target)
+				l++;
+			return l == letters.Length ? letters[0] : letters[l];
+		}
+	}
+}
