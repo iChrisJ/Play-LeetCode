285e5a24ce090b13799654371b038c85ecab2c20
iChrisJ <chrisjiang@outlook.com>
2019-08-31 13:37:40 +0800

    feat: leetcode 74, 441

---
+++ CSharp/CSharp/0074 Search a 2D Matrix.cs
@@ -0,0 +1,45 @@
+﻿namespace CSharp._0074_Search_a_2D_Matrix
+{
+	public class Solution
+	{
+		public bool SearchMatrix(int[][] matrix, int target)
+		{
+			if (matrix == null || matrix.Length == 0 || matrix[0] == null || matrix[0].Length == 0)
+				return false;
+
+			int l = 0, r = matrix.Length - 1;
+			while (l <= r)
+			{
+				int mid = l + (r - l) / 2;
+				if (matrix[mid][0] == target)
+					return true;
+				else if (matrix[mid][0] > target)
+					r = mid - 1;
+				else
+					l = mid + 1;
+			}
+
+			int row = 0;
+			if (r < 0)
+				row = l;
+			else if (l > matrix.Length - 1)
+				row = r;
+			else
+				row = matrix[l][0] > target ? r : l;
+
+			l = 0;
+			r = matrix[row].Length - 1;
+			while (l <= r)
+			{
+				int mid = l + (r - l) / 2;
+				if (matrix[row][mid] == target)
+					return true;
+				else if (matrix[row][mid] > target)
+					r = mid - 1;
+				else
+					l = mid + 1;
+			}
+			return false;
+		}
+	}
+}
---
+++ CSharp/CSharp/0441 Arranging Coins.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharp._0441_Arranging_Coins
+{
+	public class Solution
+	{
+		public int ArrangeCoins(int n)
+		{
+			if (n <= 0)
+				return 0;
+
+			long l = 1, r = n;
+			while (l <= r)
+			{
+				long i = l + (r - l) / 2;
+
+				if ((i + 1) * i / 2 <= n && (i + 1 + 1) * (i + 1) / 2 > n)
+					return (int)i;
+				else if ((i + 1) * i / 2 > n)
+					r = i - 1;
+				else
+					l = i + 1;
+			}
+			return (int)l;
+		}
+	}
+}
