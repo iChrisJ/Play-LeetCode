da64e7ddb0559f716a824fe0f6929a056802e057
iChrisJ <chrisjiang@outlook.com>
2019-09-08 16:25:50 +0800

    feat: leetcode 225, 232. stack and queue

---
+++ CSharp/CSharp/0225 Implement Stack using Queues.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp._0225_Implement_Stack_using_Queues
+{
+	public class MyStack
+	{
+		private Queue<int> queue;
+		/** Initialize your data structure here. */
+		public MyStack()
+		{
+			queue = new Queue<int>();
+		}
+
+		/** Push element x onto stack. */
+		public void Push(int x)
+		{
+			Queue<int> auxQ = new Queue<int>(queue.Count + 1);
+			auxQ.Enqueue(x);
+			while (queue.Count != 0)
+				auxQ.Enqueue(queue.Dequeue());
+
+			while (auxQ.Count != 0)
+				queue.Enqueue(auxQ.Dequeue());
+		}
+
+		/** Removes the element on top of the stack and returns that element. */
+		public int Pop()
+		{
+			return queue.Dequeue();
+		}
+
+		/** Get the top element. */
+		public int Top()
+		{
+			return queue.Peek();
+		}
+
+		/** Returns whether the stack is empty. */
+		public bool Empty()
+		{
+			return queue.Count == 0;
+		}
+	}
+}
---
+++ CSharp/CSharp/0232 Implement Queue using Stacks.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp._0232_Implement_Queue_using_Stacks
+{
+	public class MyQueue
+	{
+		private Stack<int> input;
+		private Stack<int> output;
+		/** Initialize your data structure here. */
+		public MyQueue()
+		{
+			input = new Stack<int>();
+			output = new Stack<int>();
+		}
+
+		/** Push element x to the back of queue. */
+		public void Push(int x)
+		{
+			input.Push(x);
+		}
+
+		/** Removes the element from in front of queue and returns that element. */
+		public int Pop()
+		{
+			StackTransfer();
+			return output.Pop();
+		}
+
+		/** Get the front element. */
+		public int Peek()
+		{
+			StackTransfer();
+			return output.Peek();
+		}
+
+		/** Returns whether the queue is empty. */
+		public bool Empty()
+		{
+			return input.Count == 0 && output.Count == 0;
+		}
+
+		private void StackTransfer()
+		{
+			if (output.Count == 0)
+			{
+				while (input.Count != 0)
+					output.Push(input.Pop());
+			}
+		}
+	}
+}
