732ac23d0fee2a69f4f23d40500af5c5764ddb47
iChrisJ <chrisjiang@outlook.com>
2019-09-18 22:18:32 +0800

    feat: leetcode 994, BFS; 146, Design

---
+++ CSharp/CSharp/0146 LRU Cache.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp._0146_LRU_Cache
+{
+	public class LRUCache
+	{
+		private class Node<T>
+		{
+			public Node(T value)
+			{
+				Value = value;
+			}
+
+			public Node<T> Next { get; set; }
+
+			public Node<T> Previous { get; set; }
+
+			public T Value { get; set; }
+		}
+
+		private int capacity;
+
+		private Dictionary<int, Node<(int, int)>> dict;
+		private Node<(int, int)> head;
+		private Node<(int, int)> tail;
+
+		public LRUCache(int capacity)
+		{
+			this.capacity = capacity;
+			dict = new Dictionary<int, Node<(int, int)>>(capacity);
+			head = new Node<(int, int)>((-1, -1));
+			tail = new Node<(int, int)>((-1, 1));
+			head.Next = tail;
+			tail.Previous = head;
+		}
+
+		public int Get(int key)
+		{
+			if (dict.ContainsKey(key))
+			{
+				Node<(int, int)> node = dict[key];
+
+				MoveToFirst(node);
+
+				return node.Value.Item2;
+			}
+			return -1;
+		}
+
+		public void Put(int key, int value)
+		{
+			if (dict.ContainsKey(key))
+			{
+				Node<(int, int)> node = dict[key];
+
+				MoveToFirst(node);
+
+				node.Value = (key, value);
+			}
+			else
+			{
+				if (dict.Count == capacity)
+				{
+					Node<(int, int)> last = tail.Previous;
+					tail.Previous = last.Previous;
+					last.Previous.Next = tail;
+
+					dict.Remove(last.Value.Item1);
+
+					last = null;
+				}
+				Node<(int, int)> node = new Node<(int, int)>((key, value));
+
+				head.Next.Previous = node;
+				node.Next = head.Next;
+				head.Next = node;
+				node.Previous = head;
+
+				dict.Add(key, node);
+			}
+		}
+
+		private void MoveToFirst(Node<(int, int)> node)
+		{
+			Node<(int, int)> prev = node.Previous;
+			Node<(int, int)> next = node.Next;
+
+			prev.Next = next;
+			next.Previous = prev;
+
+			head.Next.Previous = node;
+			node.Next = head.Next;
+			head.Next = node;
+			node.Previous = head;
+		}
+
+		//static void Main()
+		//{
+		//	LRUCache cache = new LRUCache(2 /* capacity */ );
+
+		//	cache.Put(1, 1);
+		//	cache.Put(2, 2);
+		//	cache.Get(1);       // returns 1
+		//	cache.Put(3, 3);    // evicts key 2
+		//	cache.Get(2);       // returns -1 (not found)
+		//	cache.Put(4, 4);    // evicts key 1
+		//	cache.Get(1);       // returns -1 (not found)
+		//	cache.Get(3);       // returns 3
+		//	cache.Get(4);       // returns 4
+		//}
+	}
+}
---
+++ CSharp/CSharp/0994 Rotting Oranges.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp._0994_Rotting_Oranges
+{
+	public class Solution
+	{
+		private int[,] direction = new int[,] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+
+		public int OrangesRotting(int[][] grid)
+		{
+			if (grid == null && grid.Length == 0)
+				throw new Exception("Incorrect parameters.");
+
+			int M = grid.Length;
+			int N = grid[0].Length;
+
+			Queue<(int, int)> queue = new Queue<(int, int)>();
+
+			for (int i = 0; i < M; i++)
+				for (int j = 0; j < N; j++)
+					if (grid[i][j] == 2)
+						queue.Enqueue((i, j));
+
+			int res = 0;
+
+			while (queue.Count != 0)
+			{
+				int levelCount = queue.Count;
+				bool hasRotten = false;
+				for (int i = 0; i < levelCount; i++)
+				{
+					(int, int) front = queue.Dequeue();
+					for (int j = 0; j < 4; j++)
+					{
+						int newx = front.Item1 + direction[j, 0];
+						int newy = front.Item2 + direction[j, 1];
+						if (newx >= 0 && newx < M && newy >= 0 && newy < N && grid[newx][newy] == 1)
+						{
+							grid[newx][newy] = 2;
+							hasRotten = true;
+							queue.Enqueue((newx, newy));
+						}
+					}
+				}
+				if (hasRotten)
+					res++;
+			}
+
+			for (int i = 0; i < M; i++)
+				for (int j = 0; j < N; j++)
+					if (grid[i][j] == 1)
+						return -1;
+			return res;
+		}
+	}
+}
