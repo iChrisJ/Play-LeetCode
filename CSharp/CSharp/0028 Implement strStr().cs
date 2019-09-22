843198ce0cfd7a32a98ad081c5c69fa5d0e916a8
iChrisJ <chrisjiang@outlook.com>
2019-09-22 17:30:33 +0800

    feat: leetcode 28. KMP algo

---
+++ CSharp/CSharp/0028 Implement strStr().cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CSharp._0028_Implement_strStr__
+{
+	public class Solution
+	{
+		public int StrStr(string haystack, string needle)
+		{
+			if (needle == null || needle.Length == 0)
+				return 0;
+
+			if (haystack == null || haystack.Length < needle.Length)
+				return -1;
+
+			for (int i = 0; i < haystack.Length - needle.Length + 1; i++)
+				if (haystack[i] == needle[0] && haystack.Substring(i, needle.Length) == needle)
+					return i;
+			return -1;
+		}
+	}
+
+	public class Solution2
+	{
+		/// KMP Algorithm
+		public int StrStr(string haystack, string needle)
+		{
+			if (needle == null || needle.Length == 0)
+				return 0;
+
+			if (haystack == null || haystack.Length < needle.Length)
+				return -1;
+
+			int[] prefix = GetPrefix(needle);
+
+			for (int i = 0, j = 0; i < haystack.Length; i++)
+			{
+				while (j > 0 && haystack[i] != needle[j])
+					j = prefix[j - 1];
+
+				if (haystack[i] == needle[j])
+					j++;
+
+				if (j == needle.Length)
+					return i - j + 1;
+			}
+			return -1;
+		}
+
+		private int[] GetPrefix(string pattern)
+		{
+			if (pattern == null || pattern.Length == 0)
+				throw new Exception("Incorrect Parameter.");
+
+			int[] prefixArr = new int[pattern.Length];
+
+			for (int i = 1, j = 0; i < pattern.Length; i++)
+			{
+				while (j > 0 && pattern[i] != pattern[j])
+					j = prefixArr[j - 1];
+
+				if (pattern[i] == pattern[j])
+					j++;
+				prefixArr[i] = j;
+			}
+			return prefixArr;
+		}
+	}
+}
---
+++ CSharp/CSharp/KMP Algorithm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CSharp.KMP_Algorithm
+{
+	class KMPAlgo
+	{
+		public int KMPSearch(string text, string pattern)
+		{
+			if (pattern == null || pattern.Length == 0)
+				return 0;
+
+			if (text == null || text.Length < pattern.Length)
+				return -1;
+
+			int[] prefixArr = GetPrefix(pattern);
+
+			for (int i = 0, j = 0; i < text.Length; i++)
+			{
+				while (j > 0 && text[i] != pattern[j])
+					j = prefixArr[j - 1];
+
+				if (text[i] == pattern[j])
+					j++;
+				if (j == pattern.Length)
+					return i - j + 1;
+			}
+			return -1;
+		}
+
+
+		private int[] GetPrefix(string pattern)
+		{
+			if (pattern == null || pattern.Length == 0)
+				throw new Exception("Incorrect Parameter.");
+
+			int[] prefixArr = new int[pattern.Length];
+
+			for (int i = 1, j = 0; i < pattern.Length; i++)
+			{
+				while (j > 0 && pattern[i] != pattern[j])
+					j = prefixArr[j - 1];
+
+				if (pattern[i] == pattern[j])
+					j++;
+				prefixArr[i] = j;
+			}
+			return prefixArr;
+		}
+	}
+}
