9478669857aca8c6ce39c1b50bb25a645ebe969e
iChrisJ <chrisjiang@outlook.com>
2019-09-24 23:35:41 +0800

    feat: leetcode 116, 117

---
+++ CSharp/CSharp/0116 Populating Next Right Pointers in Each Node.cs
@@ -0,0 +1,38 @@
+﻿namespace CSharp._0116_Populating_Next_Right_Pointers_in_Each_Node
+{
+	// Definition for a Node.
+	public class Node
+	{
+		public int val;
+		public Node left;
+		public Node right;
+		public Node next;
+
+		public Node() { }
+		public Node(int _val, Node _left, Node _right, Node _next)
+		{
+			val = _val;
+			left = _left;
+			right = _right;
+			next = _next;
+		}
+	}
+
+	public class Solution
+	{
+		public Node Connect(Node root)
+		{
+			if (root == null || root.left == null)
+				return root;
+
+			root.left.next = root.right;
+
+			if (root.next != null)
+				root.right.next = root.next.left;
+
+			Connect(root.left);
+			Connect(root.right);
+			return root;
+		}
+	}
+}
---
+++ CSharp/CSharp/0117 Populating Next Right Pointers in Each Node II.cs
@@ -0,0 +1,59 @@
+﻿namespace CSharp._0117_Populating_Next_Right_Pointers_in_Each_Node_II
+{
+	// Definition for a Node.
+	class Node
+	{
+		public int val;
+		public Node left;
+		public Node right;
+		public Node next;
+
+		public Node() { }
+
+		public Node(int _val, Node _left, Node _right, Node _next)
+		{
+			val = _val;
+			left = _left;
+			right = _right;
+			next = _next;
+		}
+	}
+
+	class Solution
+	{
+		public Node Connect(Node root)
+		{
+			if (root == null)
+				return root;
+
+			Node curNext = root.next;
+			Node cousin = null;
+
+			while (curNext != null)
+			{
+				if (curNext.left != null)
+				{
+					cousin = curNext.left;
+					break;
+				}
+
+				if (curNext.right != null)
+				{
+					cousin = curNext.right;
+					break;
+				}
+				curNext = curNext.next;
+			}
+
+			if (root.left != null)
+				root.left.next = root.right == null ? cousin : root.right;
+
+			if (root.right != null)
+				root.right.next = cousin;
+
+			Connect(root.right); // Make sure the right node it traverse first.
+			Connect(root.left);
+			return root;
+		}
+	}
+}
