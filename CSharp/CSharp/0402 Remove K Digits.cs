d0828c7bdbd6c229966b8a91dfa6f58fa8f14557
iChrisJ <chrisjiang@outlook.com>
2019-10-01 23:28:28 +0800

    feat: leetcode 402, 451

---
+++ CSharp/CSharp/0402 Remove K Digits.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp._0402_Remove_K_Digits
+{
+	public class Solution
+	{
+		public string RemoveKdigits(string num, int k)
+		{
+			if (k <= 0)
+				return num;
+
+			if (k == num.Length)
+				return "0";
+
+			Stack<int> stack = new Stack<int>();
+			StringBuilder sb = new StringBuilder(num);
+
+			for (int i = 0; i < k; i++)
+			{
+				while (sb[0] == '0')
+					sb.Remove(0, 1);
+				stack.Clear();
+				stack.Push(0);
+				for (int j = 1; j < sb.Length; j++)
+				{
+					if (sb[j] <= sb[stack.Peek()])
+						stack.Push(j);
+					else
+						break;
+				}
+				sb.Remove(stack.Peek(), 1);
+			}
+
+			while (sb.Length > 1 && sb[0] == '0')
+				sb.Remove(0, 1);
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ CSharp/CSharp/0451 Sort Characters By Frequency.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp._0451_Sort_Characters_By_Frequency
+{
+	public class Solution
+	{
+		public string FrequencySort(string s)
+		{
+			Dictionary<char, int> dict = new Dictionary<char, int>();
+			foreach (char c in s)
+			{
+				if (dict.ContainsKey(c))
+					dict[c]++;
+				else
+					dict.Add(c, 1);
+			}
+
+			char[] cArr = s.ToCharArray();
+			Array.Sort<char>(cArr,
+				(x, y) =>
+				{
+					return dict[y] == dict[x] ? y - x : dict[y] - dict[x];
+				});
+			return new string(cArr);
+		}
+	}
+}
