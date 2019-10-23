54f3d63bf5d9af027d6aafdf0c747dc2cdd8fc17
iChrisJ <chrisjiang@outlook.com>
2019-10-23 21:52:33 +0800

    feat: leetcode 542. update solution name

---
+++ CSharp/CSharp/0542 01 Matrix.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp._0542_01_Matrix
+{
+	public class Solution
+	{
+		private int[,] direction = new int[4, 2] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+
+		public int[][] UpdateMatrix(int[][] matrix)
+		{
+			if (matrix == null || matrix.Length == 0 || matrix[0] == null || matrix[0].Length == 0)
+				return matrix;
+
+			int rows = matrix.Length;
+			int cols = matrix[0].Length;
+
+			Queue<(int, int)> queue = new Queue<(int, int)>();
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					if (matrix[i][j] == 0)
+						queue.Enqueue((i, j));
+					else
+						matrix[i][j] = int.MaxValue;
+				}
+			}
+
+			while (queue.Count != 0)
+			{
+				(int, int) front = queue.Dequeue();
+
+				for (int i = 0; i < 4; i++)
+				{
+					int x = front.Item1 + direction[i, 0];
+					int y = front.Item2 + direction[i, 1];
+
+					if (x >= 0 && x < rows && y >= 0 && y < cols
+						&& matrix[x][y] > matrix[front.Item1][front.Item2] + 1)
+					{
+						matrix[x][y] = matrix[front.Item1][front.Item2] + 1;
+						queue.Enqueue((x, y));
+					}
+				}
+			}
+			return matrix;
+		}
+	}
+}
