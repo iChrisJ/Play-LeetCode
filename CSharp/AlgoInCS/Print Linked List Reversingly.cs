a19e3dc3c30ffe35c963736d9cbfb9f10b2eab03
iChrisJ <chrisjiang@outlook.com>
2019-10-27 15:41:43 +0800

    feat: PrintLinkedListReversingly

---
+++ CSharp/AlgoInCS/Print Linked List Reversingly.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 从尾到头打印链表
+/// 题目：输入一个链表的头结点，从尾到头反过来打印出每个结点的值。
+/// </summary>
+namespace AlgoInCS.Print_Linked_List_Reversingly
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int x) { val = x; }
+	}
+
+	class Solution
+	{
+		public void PrintLinkedListReversingly(ListNode head)
+		{
+			if (head == null)
+				return;
+
+			Stack<ListNode> stack = new Stack<ListNode>();
+
+			for (ListNode cur = head; cur != null; cur = cur.next)
+				stack.Push(cur);
+
+			while (stack.Count != 0)
+			{
+				ListNode top = stack.Pop();
+				Console.Write($"{top.val} ");
+			}
+		}
+	}
+
+	class Solution2
+	{
+		public void PrintLinkedListReversingly(ListNode head)
+		{
+			if (head == null)
+				return;
+
+			PrintLinkedListReversingly(head.next);
+			Console.Write($"{head.val} ");
+		}
+
+		//static void Main()
+		//{
+		//	ListNode head = new ListNode(1)
+		//	{
+		//		next = new ListNode(2)
+		//		{
+		//			next = new ListNode(3)
+		//			{
+		//				next = new ListNode(4)
+		//				{
+		//					next = new ListNode(5)
+		//				}
+		//			}
+		//		}
+		//	};
+
+		//	new Solution().PrintLinkedListReversingly(null);
+		//}
+	}
+}
---
+++ CSharp/CSharp/0050 Pow(x, n).cs
@@ -0,0 +1,39 @@
+﻿namespace CSharp._0050_Pow_x__n_
+{
+	public class Solution
+	{
+		public double MyPow(double x, int n)
+		{
+			if (n < 0)
+			{
+				x = 1 / x;
+				n = -n;
+			}
+			double res = 1;
+
+			while (n > 0)
+			{
+				if (n == 1)
+					res *= x;
+				x *= x;
+				n = n / 2;
+			}
+
+			return res;
+		}
+
+		private double Calc(double x, int n)
+		{
+			if (n == 1)
+				return x;
+			double res = Calc(x, n / 2);
+			return res * res;
+		}
+
+
+		//static void Main()
+		//{
+		//	new Solution().MyPow(2, 10);
+		//}
+	}
+}
