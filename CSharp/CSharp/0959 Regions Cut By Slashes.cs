92e52f2d1f6ea94eb56d0d119e6e42be6aa51ecf
iChrisJ <chrisjiang@outlook.com>
2019-10-30 22:49:15 +0800

    feat: leetcode 959, 1123. DFS

---
+++ CSharp/CSharp/0959 Regions Cut By Slashes.cs
@@ -0,0 +1,67 @@
+﻿namespace LeetCodeInCS._0959_Regions_Cut_By_Slashes
+{
+	public class Solution
+	{
+		private int[,] d = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+		public int RegionsBySlashes(string[] grid)
+		{
+			if (grid == null || grid.Length == 0 || grid[0] == null || grid[0].Length == 0)
+				return 0;
+
+			int N = grid.Length;
+			int[,] blocks = new int[N * 3, N * 3];
+
+			for (int i = 0; i < N; i++)
+			{
+				for (int j = 0; j < N; j++)
+				{
+					switch (grid[i][j])
+					{
+						case ' ':
+							break;
+						case '\\':
+							blocks[i * 3 + 0, j * 3 + 0] = 1;
+							blocks[i * 3 + 1, j * 3 + 1] = 1;
+							blocks[i * 3 + 2, j * 3 + 2] = 1;
+							break;
+						case '/':
+							blocks[i * 3 + 0, j * 3 + 2] = 1;
+							blocks[i * 3 + 1, j * 3 + 1] = 1;
+							blocks[i * 3 + 2, j * 3 + 0] = 1;
+							break;
+					}
+				}
+			}
+
+			int count = 0;
+			for (int i = 0; i < N * 3; i++)
+			{
+				for (int j = 0; j < N * 3; j++)
+				{
+					if (blocks[i, j] == 0)
+					{
+						DFS(blocks, i, j);
+						count++;
+					}
+				}
+			}
+			return count;
+		}
+
+		private void DFS(int[,] blocks, int x, int y)
+		{
+			blocks[x, y] = 2; // mark it as read;
+
+			int M = blocks.GetLength(0);
+
+			for (int i = 0; i < 4; i++)
+			{
+				int newx = x + d[i, 0];
+				int newy = y + d[i, 1];
+
+				if (newx >= 0 && newx < M && newy >= 0 && newy < M && blocks[newx, newy] == 0)
+					DFS(blocks, newx, newy);
+			}
+		}
+	}
+}
---
+++ CSharp/CSharp/1123 Lowest Common Ancestor of Deepest Leaves.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LeetCodeInCS._1123_Lowest_Common_Ancestor_of_Deepest_Leaves
+{
+	// Definition for a binary tree node.
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int x) { val = x; }
+	}
+
+	public class Solution
+	{
+		int max = -1;
+		TreeNode lca = null;
+
+		public TreeNode LcaDeepestLeaves(TreeNode root)
+		{
+			DFS(root, 0);
+			return lca;
+		}
+
+		private int DFS(TreeNode node, int depth)
+		{
+			if (node == null)
+				return depth - 1;
+
+			int left = DFS(node.left, depth + 1);
+			int right = DFS(node.right, depth + 1);
+
+			if (depth > max)
+				max = depth;
+
+			if (left == max && right == max)
+				lca = node;
+
+			return Math.Max(left, right);
+		}
+	}
+}
