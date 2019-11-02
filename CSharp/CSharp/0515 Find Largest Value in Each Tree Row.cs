63ad6a778330f7776e8f9fb9da277d3e028145d3
iChrisJ <chrisjiang@outlook.com>
2019-11-02 23:42:42 +0800

    feat: leetcode 515, 695. DFS, BFS

---
+++ CSharp/CSharp/0515 Find Largest Value in Each Tree Row.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeInCS._0515_Find_Largest_Value_in_Each_Tree_Row
+{
+	// Definition for a binary tree node.
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int x) { val = x; }
+	}
+
+	public class Solution
+	{
+		public IList<int> LargestValues(TreeNode root)
+		{
+			IList<int> res = new List<int>();
+
+			if (root == null)
+				return res;
+
+			Queue<TreeNode> queue = new Queue<TreeNode>();
+			queue.Enqueue(root);
+
+			while (queue.Count != 0)
+			{
+				int levelCount = queue.Count;
+
+				int max = int.MinValue;
+				for (int i = 0; i < levelCount; i++)
+				{
+					TreeNode front = queue.Dequeue();
+					max = Math.Max(max, front.val);
+
+					if (front.left != null)
+						queue.Enqueue(front.left);
+
+					if (front.right != null)
+						queue.Enqueue(front.right);
+				}
+
+				res.Add(max);
+			}
+
+			return res;
+		}
+	}
+
+	public class Solution2
+	{
+		public IList<int> LargestValues(TreeNode root)
+		{
+			IList<int> res = new List<int>();
+
+			if (root == null)
+				return res;
+
+			DFS(root, res, 0);
+			return res;
+		}
+
+		private void DFS(TreeNode node, IList<int> res, int depth)
+		{
+			if (node == null)
+				return;
+
+			if (res.Count >= depth + 1)
+				res[depth] = Math.Max(res[depth], node.val);
+			else
+				res.Add(node.val);
+
+			DFS(node.left, res, depth + 1);
+			DFS(node.right, res, depth + 1);
+		}
+	}
+}
---
+++ CSharp/CSharp/0695 Max Area of Island.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LeetCodeInCS._0695_Max_Area_of_Island
+{
+	public class Solution
+	{
+		private static int[,] d = new int[,] { { 1, 0 }, { 0, -1 }, { -1, 0 }, { 0, 1 } };
+		private bool[,] visited;
+
+		public int MaxAreaOfIsland(int[][] grid)
+		{
+			if (grid == null || grid.Length == 0 || grid[0] == null || grid[0].Length == 0)
+				return 0;
+
+			int X = grid.Length;
+			int Y = grid[0].Length;
+			visited = new bool[X, Y];
+
+			int res = 0;
+
+			for (int i = 0; i < X; i++)
+			{
+				for (int j = 0; j < Y; j++)
+				{
+					if (grid[i][j] == 1 && visited[i, j] == false)
+					{
+						int area = 0;
+						DFS(grid, i, j, X, Y, ref area);
+						res = Math.Max(res, area);
+					}
+				}
+			}
+			return res;
+		}
+
+		private void DFS(int[][] grid, int i, int j, int X, int Y, ref int area)
+		{
+			if (i >= 0 && i < X && j >= 0 && j < Y
+				&& grid[i][j] == 1 && visited[i, j] == false)
+			{
+				area++;
+				visited[i, j] = true;
+
+				for (int k = 0; k < 4; k++)
+				{
+					int newx = i + d[k, 0];
+					int newy = j + d[k, 1];
+					DFS(grid, newx, newy, X, Y, ref area);
+				}
+			}
+		}
+	}
+}
