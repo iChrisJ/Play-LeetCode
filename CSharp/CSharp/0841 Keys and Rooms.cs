d7a5d0128ee8b4bc95a0395332a5ba7aced35cc4
iChrisJ <chrisjiang@outlook.com>
2019-11-02 16:02:53 +0800

    feat: leetcode 841, 1026. DFS, BFS

---
+++ CSharp/CSharp/0841 Keys and Rooms.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeInCS._0841_Keys_and_Rooms
+{
+	public class Solution
+	{
+		public bool CanVisitAllRooms(IList<IList<int>> rooms)
+		{
+			if (rooms == null || rooms.Count == 0)
+				return false;
+
+			bool[] visited = new bool[rooms.Count];
+			Queue<int> keys = new Queue<int>();
+			keys.Enqueue(0);
+
+			while (keys.Count != 0)
+			{
+				int key = keys.Dequeue();
+				if (visited[key] == false && rooms[key] != null)
+				{
+					foreach (int item in rooms[key])
+						keys.Enqueue(item);
+					visited[key] = true;
+				}
+			}
+
+			foreach (bool v in visited)
+			{
+				if (v == false)
+					return false;
+			}
+			return true;
+		}
+	}
+
+	public class Solution2
+	{
+		public bool CanVisitAllRooms(IList<IList<int>> rooms)
+		{
+			if (rooms == null || rooms.Count == 0)
+				return false;
+
+			bool[] visited = new bool[rooms.Count];
+			visited[0] = true;
+
+			for (int i = 0; i < rooms[0].Count; i++)
+				DFS(rooms, rooms[0][i], visited);
+
+			foreach (bool v in visited)
+			{
+				if (v == false)
+					return false;
+			}
+			return true;
+		}
+
+		private void DFS(IList<IList<int>> rooms, int key, bool[] visited)
+		{
+			if (visited[key] == true)
+				return;
+
+			visited[key] = true;
+
+			for (int i = 0; rooms[key] != null && i < rooms[key].Count; i++)
+				DFS(rooms, rooms[key][i], visited);
+		}
+	}
+}
---
+++ CSharp/CSharp/1026 Maximum Difference Between Node and Ancestor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeetCodeInCS._1026_Maximum_Difference_Between_Node_and_Ancestor
+{
+
+	// Definition for a binary tree node.
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int x) { val = x; }
+	}
+
+	public class Solution
+	{
+		private int max = 0;
+		public int MaxAncestorDiff(TreeNode root)
+		{
+			return root == null ? 0 : MaxAncestorDiff(root, root.val, root.val);
+		}
+
+		private int MaxAncestorDiff(TreeNode node, int maximum, int minimum)
+		{
+			maximum = Math.Max(maximum, node.val);
+			minimum = Math.Min(minimum, node.val);
+
+			int max = Math.Max(Math.Abs(maximum - node.val), Math.Abs(minimum - node.val));
+
+			if (node.left != null)
+				max = Math.Max(max, MaxAncestorDiff(node.left, maximum, minimum));
+
+			if (node.right != null)
+				max = Math.Max(max, MaxAncestorDiff(node.right, maximum, minimum));
+
+			return max;
+		}
+	}
+}
