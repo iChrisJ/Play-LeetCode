c33fc954afbcede46164685230c31fb153788bed
iChrisJ <chrisjiang@outlook.com>
2019-11-05 22:22:46 +0800

    feat: leetcode 529, 1020. DFS, BFS

---
+++ CSharp/CSharp/0529 Minesweeper.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCodeInCS._0529_Minesweeper
+{
+	public class Solution
+	{
+		private static int[,] d = new int[,] { { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
+		private static bool[,] visited;
+
+		public char[][] UpdateBoard(char[][] board, int[] click)
+		{
+			int X = board.Length;
+			int Y = board[0].Length;
+			char[][] res = new char[X][];
+			visited = new bool[X, Y];
+
+			for (int i = 0; i < X; i++)
+			{
+				res[i] = new char[Y];
+				for (int j = 0; j < Y; j++)
+					res[i][j] = board[i][j];
+			}
+
+			int x = click[0], y = click[1];
+
+			if (board[x][y] == 'M')
+				res[x][y] = 'X';
+			else if (board[x][y] == 'E')
+				DFS(board, res, x, y);
+
+			return res;
+		}
+
+		private void DFS(char[][] board, char[][] res, int x, int y)
+		{
+			int X = board.Length;
+			int Y = board[0].Length;
+
+			if (board[x][y] == 'E' && visited[x, y] == false)
+			{
+				visited[x, y] = true;
+				int mineCount = 0;
+
+				for (int i = 0; i < 8; i++)
+				{
+					int newx = x + d[i, 0];
+					int newy = y + d[i, 1];
+
+					if (newx >= 0 && newx < X && newy >= 0 && newy < Y && board[newx][newy] == 'M')
+						mineCount++;
+				}
+
+				if (mineCount > 0)
+					res[x][y] = char.Parse(mineCount.ToString());
+				else
+				{
+					res[x][y] = 'B';
+					for (int i = 0; i < 8; i++)
+					{
+						int newx = x + d[i, 0];
+						int newy = y + d[i, 1];
+
+						if (newx >= 0 && newx < X && newy >= 0 && newy < Y && board[newx][newy] == 'E' && !visited[newx, newy])
+							DFS(board, res, newx, newy);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ CSharp/CSharp/1020 Number of Enclaves.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeInCS._1020_Number_of_Enclaves
+{
+	public class Solution
+	{
+		private static int[,] d = new int[,] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+		public int NumEnclaves(int[][] A)
+		{
+			if (A == null || A.Length < 1 || A[0] == null || A[0].Length < 1)
+				throw new ArgumentException("Invalid parameter.");
+
+			int X = A.Length, Y = A[0].Length;
+
+			for (int i = 0; i < X; i++)
+			{
+				for (int j = 0; j < Y; j++)
+				{
+					if (((i == 0) || (i == X - 1) || (j == 0) || (j == Y - 1)) && A[i][j] == 1)
+					{
+						A[i][j] = 2;
+						DFS(A, i, j);
+					}
+				}
+			}
+
+			int res = 0;
+			for (int i = 0; i < X; i++)
+			{
+				for (int j = 0; j < Y; j++)
+				{
+					if (A[i][j] == 1)
+						res++;
+				}
+			}
+			return res;
+		}
+
+		private void DFS(int[][] A, int x, int y)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				int newx = x + d[i, 0], newy = y + d[i, 1];
+				if (newx >= 0 && newx < A.Length && newy >= 0 && newy < A[0].Length && A[newx][newy] == 1)
+				{
+					A[newx][newy] = 2;
+					DFS(A, newx, newy);
+				}
+			}
+		}
+	}
+
+	public class Solution2
+	{
+		private static int[,] d = new int[,] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+		public int NumEnclaves(int[][] A)
+		{
+			if (A == null || A.Length < 1 || A[0] == null || A[0].Length < 1)
+				throw new ArgumentException("Invalid parameter.");
+
+			int X = A.Length, Y = A[0].Length;
+
+			Queue<(int, int)> queue = new Queue<(int, int)>();
+
+			for (int i = 0; i < X; i++)
+			{
+				for (int j = 0; j < Y; j++)
+				{
+					if (((i == 0) || (i == X - 1) || (j == 0) || (j == Y - 1)) && A[i][j] == 1)
+						queue.Enqueue((i, j));
+				}
+			}
+
+			while (queue.Count != 0)
+			{
+				(int, int) front = queue.Dequeue();
+
+				A[front.Item1][front.Item2] = 2;
+				for (int i = 0; i < 4; i++)
+				{
+					int newx = front.Item1 + d[i, 0], newy = front.Item2 + d[i, 1];
+					if (newx >= 0 && newx < A.Length && newy >= 0 && newy < A[0].Length && A[newx][newy] == 1)
+						queue.Enqueue((newx, newy));
+				}
+
+			}
+
+			int res = 0;
+			for (int i = 0; i < X; i++)
+			{
+				for (int j = 0; j < Y; j++)
+				{
+					if (A[i][j] == 1)
+						res++;
+				}
+			}
+			return res;
+		}
+	}
+}
