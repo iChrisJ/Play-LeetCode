f0f9f400cc41a3b283b421979a765fbc5ecb6d79
iChrisJ <chrisjiang@outlook.com>
2019-12-20 22:44:57 +0800

    feat: leetcode 42, 653.

---
+++ CSharp/CSharp/0042 Trapping Rain Water.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace LeetCodeInCS._0042_Trapping_Rain_Water
+{
+	public class Solution
+	{
+		public int Trap(int[] height)
+		{
+			if (height == null || height.Length <= 2)
+				return 0;
+
+			int res = 0;
+			for (int i = 0; i < height.Length; i++)
+			{
+				int max_left = 0;
+				for (int l = i; l >= 0; l--)
+					max_left = Math.Max(max_left, height[l]);
+
+				int max_right = 0;
+				for (int r = i; r < height.Length; r++)
+					max_right = Math.Max(max_right, height[r]);
+				res += Math.Min(max_left, max_right) - height[i];
+			}
+			return res;
+		}
+	}
+
+	/// <summary>
+	/// Store left_most and right_most of each height in extra arrays.
+	/// </summary>
+	public class Solution2
+	{
+		public int Trap(int[] height)
+		{
+			if (height == null || height.Length <= 2)
+				return 0;
+
+			int[] max_left = new int[height.Length];
+			max_left[0] = height[0];
+			for (int i = 1; i < height.Length; i++)
+				max_left[i] = Math.Max(max_left[i - 1], height[i]);
+
+			int[] max_right = new int[height.Length];
+			max_right[height.Length - 1] = height[height.Length - 1];
+			for (int i = height.Length - 2; i >= 0; i--)
+				max_right[i] = Math.Max(max_right[i + 1], height[i]);
+
+			int res = 0;
+			for (int i = 0; i < height.Length; i++)
+				res += Math.Min(max_left[i], max_right[i]) - height[i];
+			return res;
+		}
+	}
+
+
+	/// <summary>
+	/// Two Points
+	/// </summary>
+	public class Solution3
+	{
+		public int Trap(int[] height)
+		{
+			if (height == null || height.Length <= 2)
+				return 0;
+
+			int l = 0, r = height.Length - 1;
+			int lMost = 0, rMost = 0;
+			int res = 0;
+
+			while (l < r)
+			{
+				if (height[l] <= height[r])
+				{
+					if (lMost < height[l])
+						lMost = height[l];
+					else
+						res += lMost - height[l];
+					l++;
+				}
+				else
+				{
+					if (rMost < height[r])
+						rMost = height[r];
+					else
+						res += rMost - height[r];
+					r--;
+				}
+			}
+			return res;
+		}
+	}
+}
---
+++ CSharp/CSharp/0653 Two Sum IV - Input is a BST.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeInCS._0653_Two_Sum_IV___Input_is_a_BST
+{
+	// Definition for a binary tree node.
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int x) { val = x; }
+	}
+
+	/// <summary>
+	/// InOrder to convert the tree to a sorted array, then traverse the array with two points
+	/// </summary>
+	public class Solution
+	{
+		public bool FindTarget(TreeNode root, int k)
+		{
+			if (root == null)
+				return false;
+
+			List<int> arr = new List<int>();
+			InOrder(root, arr);
+
+			for (int i = 0, j = arr.Count - 1; i < j;)
+			{
+				if (arr[i] + arr[j] > k)
+					j--;
+				else if (arr[i] + arr[j] < k)
+					i++;
+				else
+					return true;
+			}
+			return false;
+		}
+
+		private void InOrder(TreeNode node, List<int> arr)
+		{
+			if (node == null)
+				return;
+
+			InOrder(node.left, arr);
+			arr.Add(node.val);
+			InOrder(node.right, arr);
+		}
+	}
+
+	/// <summary>
+	/// DFS to traverse the tree and store the traversed node in Set.
+	/// </summary>
+	public class Solution2
+	{
+		public bool FindTarget(TreeNode root, int k)
+		{
+			if (root == null)
+				return false;
+
+			HashSet<int> set = new HashSet<int>();
+			return DFS(root, set, k);
+		}
+
+		private bool DFS(TreeNode node, HashSet<int> set, int k)
+		{
+			if (node == null)
+				return false;
+
+			if (set.Contains(k - node.val))
+				return true;
+			else
+			{
+				set.Add(node.val);
+				return DFS(node.left, set, k) || DFS(node.right, set, k);
+			}
+		}
+	}
+}
