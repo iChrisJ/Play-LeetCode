84005a9c5eafbebb25ca0d5c52cc13f6eeb6b18f
iChrisJ <chrisjiang@outlook.com>
2019-12-26 23:04:16 +0800

    feat: update leetcode 239

--- CSharp/CSharp/0239 Sliding Window Maximum.cs
+++ CSharp/CSharp/0239 Sliding Window Maximum.cs
@@ -1,50 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace LeetCodeInCS._0239_Sliding_Window_Maximum
 {
-
 	public class Solution
 	{
 		public int[] MaxSlidingWindow(int[] nums, int k)
 		{
-			if (nums == null || nums.Length < k)
-				throw new Exception("Incorrect parameters.");
-
-			if (nums.Length == 0 || k <= 0)
+			if (k == 0)
 				return new int[0];
 
-			LinkedList<int> queue = new LinkedList<int>();
 			int[] res = new int[nums.Length - k + 1];
+			int l = 0, r = 0;
+			int curMaxInd = 0;
+			int start = 0;
 
-			for (int i = 0; i < k - 1; i++)
+			while (r < nums.Length)
 			{
-				while (queue.Count != 0 && queue.First.Value < nums[i])
-					queue.RemoveFirst();
-				queue.AddLast(nums[i]);
-			}
+				curMaxInd = nums[curMaxInd] < nums[r] ? r : curMaxInd;
+				r++;
 
-			for (int i = k - 1; i < nums.Length; i++)
-			{
-				while ((queue.Count != 0 && (queue.First.Value < nums[i] || queue.First.Value < GetMaxValue(queue))) || queue.Count == k)
-					queue.RemoveFirst();
-				queue.AddLast(nums[i]);
-				res[i - k + 1] = queue.First.Value;
-			}
-			return res;
-		}
+				if (l > curMaxInd)
+				{
+					curMaxInd = l;
+					for (int i = l + 1; i < r; i++)
+						curMaxInd = nums[curMaxInd] < nums[i] ? i : curMaxInd;
+				}
 
-		private int GetMaxValue(LinkedList<int> queue)
-		{
-			if (queue == null || queue.Count == 0)
-				return int.MinValue;
-			int res = queue.First.Value;
-			LinkedListNode<int> cur = queue.First;
-			while (cur != null)
-			{
-				if (cur.Value > res)
-					res = cur.Value;
-				cur = cur.Next;
+				if (r - l == k)
+				{
+					res[start++] = nums[curMaxInd];
+					l++;
+				}
 			}
 			return res;
 		}
@@ -52,75 +38,38 @@ namespace LeetCodeInCS._0239_Sliding_Window_Maximum
 
 	public class Solution2
 	{
-		// TODO
 		public int[] MaxSlidingWindow(int[] nums, int k)
 		{
-			if (nums == null || nums.Length < k)
-				throw new Exception("Incorrect parameters.");
-
-			if (nums.Length == 0 || k <= 0)
+			if (nums == null || nums.Length * k == 0)
 				return new int[0];
 
-			throw new NotImplementedException();
+			int[] res = new int[nums.Length - k + 1];
+			LinkedList<int> deque = new LinkedList<int>();
+
+			for (int i = 0; i < k; i++)
+			{
+				CleanUp_Deque(nums, deque, i, k);
+				deque.AddLast(i);
+			}
+
+			res[0] = nums[deque.First.Value];
+
+			for (int i = k; i < nums.Length; i++)
+			{
+				CleanUp_Deque(nums, deque, i, k);
+				deque.AddLast(i);
+				res[i - k + 1] = nums[deque.First.Value];
+			}
+			return res;
 		}
 
-		private class MaxHeap
+		private void CleanUp_Deque(int[] nums, LinkedList<int> deque, int i, int k)
 		{
-			private List<int> data;
+			if (deque.Count > 0 && deque.First.Value == i - k)
+				deque.RemoveFirst();
 
-			public MaxHeap()
-			{
-				data = new List<int>();
-			}
-
-			public MaxHeap(int capacity)
-			{
-				data = new List<int>(capacity);
-			}
-
-			public void Push(int item)
-			{
-				data.Add(item);
-				ShiftUp(data.Count - 1);
-			}
-
-			private void ShiftUp(int i)
-			{
-				while (i - 1 >= 0 && data[i] > data[(i - 1) / 2])
-				{
-					int temp = data[i];
-					data[i] = data[(i - 1) / 2];
-					data[(i - 1) / 2] = temp;
-					i = (i - 1) / 2;
-				}
-			}
-
-			public int Pop()
-			{
-				int res = data[0];
-				data[0] = data[data.Count - 1];
-				data.RemoveAt(data.Count - 1);
-				ShiftDown(0);
-
-				return res;
-			}
-
-			private void ShiftDown(int i)
-			{
-				while (2 * i + 1 < data.Count)
-				{
-					int p = 2 * i + 1;
-					if (p + 1 < data.Count && data[p + 1] > data[p])
-						p = p + 1;
-					if (data[p] >= data[i])
-						break;
-
-					int temp = data[p];
-					data[p] = data[i];
-					data[i] = i;
-					i = p;
-				}
-			}
+			while (deque.Count > 0 && nums[i] >= nums[deque.Last.Value])
+				deque.RemoveLast();
 		}
 	}
 }
