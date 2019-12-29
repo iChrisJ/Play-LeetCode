0ce6429c9518e16fac2b858fbb4ef73a830771fb
iChrisJ <chrisjiang@outlook.com>
2019-12-29 19:09:05 +0800

    feat: leetcode 992, 995. sliding window

---
+++ CSharp/CSharp/0992 Subarrays with K Different Integers.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeInCS._0992_Subarrays_with_K_Different_Integers
+{
+	// Brute Force
+	public class Solution
+	{
+		public int SubarraysWithKDistinct(int[] A, int K)
+		{
+			if (A == null || A.Length == 0 || A.Length < K)
+				return 0;
+
+			int l = 0, r = 0;
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+			int res = 0;
+
+			while (l <= A.Length - K)
+			{
+				while (dict.Count <= K && r < A.Length)
+				{
+					if (dict.ContainsKey(A[r]))
+						dict[A[r]]++;
+					else
+						dict.Add(A[r], 1);
+
+					if (dict.Count == K)
+						res++;
+					r++;
+				}
+
+				dict = new Dictionary<int, int>();
+				l++;
+				r = l;
+			}
+			return res;
+		}
+	}
+
+	public class Solution2
+	{
+		public int SubarraysWithKDistinct(int[] A, int K)
+		{
+			return SubarraysWithAtMostKDistinct(A, K) - SubarraysWithAtMostKDistinct(A, K - 1);
+		}
+
+		private int SubarraysWithAtMostKDistinct(int[] A, int k)
+		{
+			int l = 0, res = 0;
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+
+			for (int r = 0; r < A.Length; r++)
+			{
+				if (!dict.ContainsKey(A[r]))
+				{
+					dict.Add(A[r], 1);
+					k--;
+				}
+				else
+					dict[A[r]]++;
+
+				while (k < 0)
+				{
+					dict[A[l]]--;
+					if (dict[A[l]] == 0)
+					{
+						dict.Remove(A[l]);
+						k++;
+					}
+					l++;
+				}
+				res += r - l + 1;
+			}
+			return res;
+		}
+	}
+}
---
+++ CSharp/CSharp/0995 Minimum Number of K Consecutive Bit Flips.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCodeInCS._0995_Minimum_Number_of_K_Consecutive_Bit_Flips
+{
+	/// <summary>
+	/// Brute Force
+	/// Time Limit Exceeded
+	/// </summary>
+	public class Solution
+	{
+		public int MinKBitFlips(int[] A, int K)
+		{
+			if (A == null || A.Length == 0 || K == 0)
+				return -1;
+
+			int res = 0;
+
+			for (int i = 0; i < A.Length; i++)
+			{
+				if (A[i] == 0)
+				{
+					if (i <= A.Length - K)
+					{
+						for (int j = i; j < i + K; j++)
+							A[j] ^= 1;
+						res++;
+					}
+					else
+						return -1;
+				}
+			}
+			return res;
+		}
+	}
+
+	public class Solution2
+	{
+		public int MinKBitFlips(int[] A, int K)
+		{
+			if (A == null || A.Length == 0 || K == 0)
+				return -1;
+
+			int[] flipped = new int[A.Length];
+			int flip = 0;
+			int res = 0;
+
+			for (int i = 0; i < A.Length; i++)
+			{
+				if (i >= K)
+					flip ^= flipped[i - K];
+				if ((A[i] ^ flip) == 0)
+				{
+					if (i + K > A.Length)
+						return -1;
+					res++;
+					flip ^= 1;
+					flipped[i] = 1;
+				}
+			}
+			return res;
+		}
+	}
+}
