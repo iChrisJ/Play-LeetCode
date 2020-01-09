26c014afabeb5a706afe1269f95b625db31c6131
iChrisJ <chrisjiang@outlook.com>
2020-01-09 23:04:30 +0800

    feat: leetcode 155, 279, 752

---
+++ CSharp/CSharp/0155 Min Stack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeInCS._0155_Min_Stack
+{
+	public class MinStack
+	{
+		Stack<int> data;
+		Stack<int> mins;
+
+		public MinStack()
+		{
+			data = new Stack<int>();
+			mins = new Stack<int>();
+		}
+
+		public void Push(int x)
+		{
+			data.Push(x);
+			if (mins.Count == 0 || mins.Peek() >= x)
+				mins.Push(x);
+		}
+
+		public void Pop()
+		{
+			int top = data.Pop();
+
+			if (top == mins.Peek())
+				mins.Pop();
+		}
+
+		public int Top()
+		{
+			return data.Peek();
+		}
+
+		public int GetMin()
+		{
+			return mins.Peek();
+		}
+	}
+}
--- CSharp/CSharp/0279 Perfect Squares.cs
+++ CSharp/CSharp/0279 Perfect Squares.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LeetCodeInCS._0279_Perfect_Squares
 {
@@ -62,16 +63,52 @@ namespace LeetCodeInCS._0279_Perfect_Squares
 		{
 			if (n <= 0)
 				return 0;
-			int[] memo = new int[n + 1];
-
-			for (int i = 0; i <= n; i++)
-				memo[i] = i;
+			int[] dp = new int[n + 1];
 
 			for (int i = 1; i <= n; i++)
+			{
+				dp[i] = i;
 				for (int j = 1; j * j <= i; j++)
-					memo[i] = Math.Min(memo[i], 1 + memo[i - j * j]);
+					dp[i] = Math.Min(dp[i], 1 + dp[i - j * j]);
+			}
 
-			return memo[n];
+			return dp[n];
+		}
+	}
+
+	/// BFS
+	/// Time Complexity: O(nlgn)
+	/// Space Complexity: O(n)
+	public class Solution4
+	{
+		public int NumSquares(int n)
+		{
+			Queue<int> queue = new Queue<int>();
+			HashSet<int> set = new HashSet<int>();
+			queue.Enqueue(n);
+			set.Add(n);
+
+			int ans = 0;
+
+			while (queue.Count != 0)
+			{
+				ans++;
+				int len = queue.Count;
+				while ((len--) > 0)
+				{
+					int frnt = queue.Dequeue();
+					for (int i = 1; i * i <= n; i++)
+					{
+						int res = frnt - i * i;
+						if (res == 0)
+							return ans;
+
+						if (set.Add(res))
+							queue.Enqueue(res);
+					}
+				}
+			}
+			return n;
 		}
 	}
 }
