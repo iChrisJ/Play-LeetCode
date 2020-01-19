156c24a6f47a9d3404be193f91a07567a66685ee
iChrisJ <chrisjiang@outlook.com>
2020-01-19 16:32:56 +0800

    feat: leetcode 23, 234. Linked List

---
+++ CSharp/CSharp/0023 Merge k Sorted Lists.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeInCS._0023_Merge_k_Sorted_Lists
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int x) { val = x; }
+	}
+
+	public class Solution
+	{
+		public ListNode MergeKLists(ListNode[] lists)
+		{
+			if (lists == null || lists.Length == 0)
+				return null;
+
+			Queue<ListNode> queue = new Queue<ListNode>(lists);
+			while (queue.Count > 1)
+			{
+				ListNode l1 = queue.Dequeue();
+				ListNode l2 = queue.Dequeue();
+				queue.Enqueue(Merge(l1, l2));
+			}
+			return queue.Peek();
+		}
+
+		private ListNode Merge(ListNode l1, ListNode l2)
+		{
+			ListNode dummy = new ListNode(-1);
+			ListNode prev = dummy;
+
+			while (l1 != null || l2 != null)
+			{
+				if (l1 == null)
+				{
+					prev.next = l2;
+					break;
+				}
+				else if (l2 == null)
+				{
+					prev.next = l1;
+					break;
+				}
+				else if (l1.val < l2.val)
+				{
+					prev.next = l1;
+					l1 = l1.next;
+				}
+				else
+				{
+					prev.next = l2;
+					l2 = l2.next;
+				}
+				prev = prev.next;
+				prev.next = null;
+			}
+			return dummy.next;
+		}
+	}
+}
---
+++ CSharp/CSharp/0234 Palindrome Linked List.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeInCS._0234_Palindrome_Linked_List
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int x) { val = x; }
+	}
+
+	public class Solution
+	{
+		public bool IsPalindrome(ListNode head)
+		{
+			if (head == null || head.next == null)
+				return true;
+
+			ListNode mid = head, end = head;
+
+			while (end != null && end.next != null)
+			{
+				mid = mid.next;
+				end = end.next.next;
+			}
+
+			if (end != null)
+				mid = mid.next;
+
+			// reverse the right part LinkedList.
+			ListNode pre = null;
+			while (mid != null)
+			{
+				ListNode next = mid.next;
+				mid.next = pre;
+				pre = mid;
+				mid = next;
+			}
+
+			end = head;
+			while (end != null && pre != null)
+			{
+				if (end.val != pre.val)
+					return false;
+				pre = pre.next;
+				end = end.next;
+			}
+			return true;
+		}
+	}
+
+	public class Solution2
+	{
+		public bool IsPalindrome(ListNode head)
+		{
+			if (head == null || head.next == null)
+				return true;
+
+			ListNode behind = head, front = head;
+			while (front != null && front.next != null)
+			{
+				behind = behind.next;
+				front = front.next.next;
+			}
+
+			front = front == null ? behind : behind.next;
+
+			Stack<ListNode> stack = new Stack<ListNode>();
+			while (front != null)
+			{
+				stack.Push(front);
+				front = front.next;
+			}
+
+			while (stack.Count > 0)
+			{
+				if (head.val != stack.Pop().val)
+					return false;
+				head = head.next;
+			}
+			return true;
+		}
+	}
+}
