9b29ba35fe85a13c0a64f14413daeb5719d7fcc9
iChrisJ <chrisjiang@outlook.com>
2020-02-01 23:12:08 +0800

    feat: leetcode. word ladder

---
+++ CSharp/CSharp/0126 Word Ladder II.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeInCS._0126_Word_Ladder_II
+{
+	// TODO: Reduce time complexity.
+	public class Solution
+	{
+		private IList<IList<string>> res;
+		private int minStep = int.MaxValue;
+		public IList<IList<string>> FindLadders(string beginWord, string endWord, IList<string> wordList)
+		{
+			res = new List<IList<string>>();
+			if (wordList == null || wordList.Count == 0)
+				return res;
+
+			HashSet<string> set = new HashSet<string>(wordList);
+			if (!set.Contains(endWord))
+				return res;
+
+			Dictionary<string, HashSet<string>> dict = GetWordDict(beginWord, endWord, set);
+			HashSet<string> visited = new HashSet<string> { beginWord };
+			DFS(beginWord, endWord, dict, visited, new List<string> { beginWord }, 0);
+
+			IList<IList<string>> ans = new List<IList<string>>();
+			foreach (IList<string> path in res)
+			{
+				if (path.Count == minStep)
+					ans.Add(path);
+			}
+			return ans;
+		}
+
+		private void DFS(string curWord, string endWord, Dictionary<string, HashSet<string>> dict, HashSet<string> visited, IList<string> path, int step)
+		{
+			if (curWord == endWord)
+			{
+				minStep = Math.Min(minStep, step);
+				if (step == minStep)
+					res.Add(new List<string>(path));
+				return;
+			}
+
+			HashSet<string> vals = dict[curWord];
+			foreach (string val in vals)
+			{
+				if (!visited.Contains(val))
+				{
+					visited.Add(val);
+					path.Add(val);
+					DFS(val, endWord, dict, visited, path, step + 1);
+					visited.Remove(val);
+					path.RemoveAt(path.Count - 1);
+				}
+			}
+		}
+
+		private Dictionary<string, HashSet<string>> GetWordDict(string beginWord, string endWord, HashSet<string> set)
+		{
+			Dictionary<string, HashSet<string>> dict = new Dictionary<string, HashSet<string>>();
+			List<string> list = new List<string> { beginWord };
+			list.AddRange(set);
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (list[i] == endWord || dict.ContainsKey(list[i]))
+					continue;
+
+				HashSet<string> val_set = new HashSet<string>();
+
+				for (int j = 0; j < list[i].Length; j++)
+				{
+					if (list[i] == endWord)
+						continue;
+					for (char k = 'a'; k < 'z'; k++)
+					{
+						if (k == list[i][j])
+							continue;
+
+						string updated = $"{list[i].Substring(0, j)}{k}{list[i].Substring(j + 1)}";
+						if (set.Contains(updated))
+							val_set.Add(updated);
+					}
+				}
+				dict.Add(list[i], val_set);
+			}
+			return dict;
+		}
+	}
+}
---
+++ CSharp/CSharp/0127 Word Ladder.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeInCS._0127_Word_Ladder
+{
+	// TODO: Reduce time complexity.
+	public class Solution
+	{
+		public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+		{
+			if (wordList == null || wordList.Count == 0)
+				return 0;
+
+			Dictionary<string, HashSet<string>> dict = GetWordDict(beginWord, endWord, wordList);
+			HashSet<string> visited = new HashSet<string> { beginWord };
+			int trans_time = 0;
+
+			Queue<string> queue = new Queue<string>();
+			queue.Enqueue(beginWord);
+			while (queue.Count > 0)
+			{
+				trans_time++;
+				int len = queue.Count;
+				while ((len--) > 0)
+				{
+					string frnt = queue.Dequeue();
+					if (frnt == endWord)
+						return trans_time;
+
+					HashSet<string> val_list = dict[frnt];
+					foreach (string val in val_list)
+						if (visited.Add(val))
+							queue.Enqueue(val);
+				}
+			}
+			return 0;
+		}
+
+		private Dictionary<string, HashSet<string>> GetWordDict(string beginWord, string endWord, IList<string> wordList)
+		{
+			Dictionary<string, HashSet<string>> dict = new Dictionary<string, HashSet<string>>();
+			List<string> list = new List<string> { beginWord };
+			list.AddRange(wordList);
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (list[i] == endWord || dict.ContainsKey(list[i]))
+					continue;
+
+				HashSet<string> val_set = new HashSet<string>();
+				for (int j = 0; j < list.Count; j++)
+				{
+					if (IsOneCharDiff(list[i], list[j]))
+						val_set.Add(list[j]);
+				}
+				dict.Add(list[i], val_set);
+			}
+			return dict;
+		}
+
+		private bool IsOneCharDiff(string key, string val)
+		{
+			if (key == null || val == null || key.Length != val.Length || key == val)
+				return false;
+
+			int diff_Count = 0;
+			for (int i = 0; i < key.Length; i++)
+			{
+				if (key[i] != val[i])
+					diff_Count++;
+			}
+			return diff_Count == 1;
+		}
+
+		public class Solution2
+		{
+			public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+			{
+				if (wordList == null || wordList.Count == 0)
+					return 0;
+
+				HashSet<string> set = new HashSet<string>(wordList);
+				if (!set.Contains(endWord))
+					return 0;
+
+				int step = 0;
+				HashSet<string> visited = new HashSet<string> { beginWord };
+				Queue<string> queue = new Queue<string>();
+				queue.Enqueue(beginWord);
+
+				while (queue.Count > 0)
+				{
+					step++;
+					int len = queue.Count;
+					while ((len--) > 0)
+					{
+						string frnt = queue.Dequeue();
+						if (frnt == endWord)
+							return step;
+
+						for (int i = 0; i < frnt.Length; i++)
+						{
+							for (char j = 'a'; j <= 'z'; j++)
+							{
+								if (j == frnt[i])
+									continue;
+								string updated = $"{frnt.Substring(0, i)}{j}{frnt.Substring(i + 1)}";
+
+								// one optimization
+								if (updated == endWord)
+									return step + 1;
+
+								if (set.Contains(updated) && visited.Add(updated))
+									queue.Enqueue(updated);
+							}
+						}
+					}
+				}
+				return 0;
+			}
+		}
+	}
+}
+
