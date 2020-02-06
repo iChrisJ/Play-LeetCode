2bf8b860ee53f0ca298302f7f2ca7bce0693499b
iChrisJ <chrisjiang@outlook.com>
2020-02-06 22:56:42 +0800

    feat: leetcode, recursive and backtracking

--- CSharp/CSharp/0017 Letter Combinations of a Phone Number.cs
+++ CSharp/CSharp/0017 Letter Combinations of a Phone Number.cs
@@ -4,58 +4,43 @@ namespace LeetCodeInCS._0017_Letter_Combinations_of_a_Phone_Number
 {
 	public class Solution
 	{
-		// Time Complexity: O(3^n) = O(2^n)
-		public IList<string> Result { get; private set; }
+		private Dictionary<char, string> dict;
+
+		private IList<string> res;
 
 		public IList<string> LetterCombinations(string digits)
 		{
-			Result = new List<string>();
-			if (string.IsNullOrEmpty(digits))
-				return Result;
-			FindLetterCombinations(digits, 0, string.Empty);
-			return Result;
+			res = new List<string>();
+			if (digits == null || digits.Length == 0)
+				return res;
+
+			dict = new Dictionary<char, string>
+			{
+				{'2', "abc"},
+				{'3', "def"},
+				{'4', "ghi"},
+				{'5', "jkl"},
+				{'6', "mno"},
+				{'7', "pqrs"},
+				{'8', "tuv"},
+				{'9', "wxyz"}
+			};
+
+			GenerateCombination(digits, 0, string.Empty);
+			return res;
 		}
 
-		private void FindLetterCombinations(string digits, int index, string str)
+		// DFS
+		private void GenerateCombination(string digits, int index, string cur_combine)
 		{
 			if (index == digits.Length)
 			{
-				Result.Add(str);
+				res.Add(cur_combine);
 				return;
 			}
 
-			char c = digits[index];
-			string letters = LetterMap(c);
-			for (int i = 0; i < letters.Length; i++)
-				FindLetterCombinations(digits, index + 1, str + letters[i]);
-		}
-
-		private string LetterMap(char c)
-		{
-			switch (c)
-			{
-				case '0':
-					return " ";
-				case '2':
-					return "abc";
-				case '3':
-					return "def";
-				case '4':
-					return "ghi";
-				case '5':
-					return "jkl";
-				case '6':
-					return "mno";
-				case '7':
-					return "pqrs";
-				case '8':
-					return "tuv";
-				case '9':
-					return "wxyz";
-				case '1':
-				default:
-					return "";
-			}
+			for (int i = 0; i < dict[digits[index]].Length; i++)
+				GenerateCombination(digits, index + 1, cur_combine + dict[digits[index]][i]);
 		}
 	}
 }
--- CSharp/CSharp/0093 Restore IP Addresses.cs
+++ CSharp/CSharp/0093 Restore IP Addresses.cs
@@ -4,41 +4,36 @@ namespace LeetCodeInCS._0093_Restore_IP_Addresses
 {
 	public class Solution
 	{
-
-		public IList<string> Result { get; private set; }
+		private IList<string> res;
 
 		public IList<string> RestoreIpAddresses(string s)
 		{
-			Result = new List<string>();
-			GetValidIPs(string.Empty, s, 0);
-			return Result;
+			res = new List<string>();
+			if (s == null || s.Length < 4 || s.Length > 12)
+				return res;
+			BuildIPAddresses(string.Empty, s, 0);
+			return res;
 		}
 
-		private void GetValidIPs(string first, string rest, int dotCount)
+		private void BuildIPAddresses(string begin, string rest, int dotCount)
 		{
 			if (dotCount == 3)
 			{
-				if (rest.Length > 1 && rest[0] == '0')
+				if (rest.Length == 0 || (rest.Length > 1 && rest[0] == '0') || int.Parse(rest) > 255)
 					return;
-				if (rest.Length > 0 && int.Parse(rest) <= 255)
-					Result.Add(first + rest);
+				res.Add($"{begin}{rest}");
 				return;
 			}
 
 			for (int i = 1; i <= 3 && i <= rest.Length; i++)
 			{
-				string cur = rest.Substring(0, i);
-				if (cur.Length > 1 && cur[0] == '0')
+				string segment = rest.Substring(0, i);
+				if (segment.Length > 1 && segment[0] == '0') // 剪枝
 					return;
 
-				if (int.Parse(cur) <= 255 || (int.Parse(cur) == 0 && cur.Length == 1))
-					GetValidIPs(first + cur + ".", rest.Substring(i), dotCount + 1);
+				if (int.Parse(segment) <= 255)
+					BuildIPAddresses($"{begin}{segment}.", rest.Substring(i), dotCount + 1);
 			}
 		}
-
-		//public static void Main(string[] args)
-		//{
-		//	var a = new Solution().RestoreIpAddresses("010010");
-		//}
 	}
 }
