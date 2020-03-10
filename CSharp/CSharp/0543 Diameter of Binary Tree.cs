7be8d41b1723aa3627fe9796a9dfc925c739c530
iChrisJ <chrisjiang@outlook.com>
2020-03-10 23:29:53 +0800

    feat: leetcode. tree

---
+++ CSharp/CSharp/0543 Diameter of Binary Tree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCodeInCS._0543_Diameter_of_Binary_Tree
+{
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int x) { val = x; }
+	}
+
+	public class Solution
+	{
+		private int diameter = 0;
+		public int DiameterOfBinaryTree(TreeNode root)
+		{
+			Depth(root);
+			return diameter;
+		}
+
+		private int Depth(TreeNode node)
+		{
+			if (node == null)
+				return 0;
+
+			int left = Depth(node.left);
+			int right = Depth(node.right);
+			diameter = Math.Max(diameter, left + right);
+			return Math.Max(left, right) + 1;
+		}
+	}
+}
